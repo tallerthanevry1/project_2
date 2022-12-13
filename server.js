dd2f2705ad172b527d051c0f926e468bfb0a206c
Brendan Doherty <brendandoherty@Brendans-MBP.lan>
2022-12-13 16:57:37 -0500

    final final 1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyparser = require("body-parser");
-const path = require('path');
+const path = require('path');  
 
 const connectDB = require('./server/database/connection');
 
